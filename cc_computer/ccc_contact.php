3e1306033357298adde3954996d903c2f44b8e12
TurtleGITHUB <76065011+TurtleGITHUB@users.noreply.github.com>
2021-06-14 09:59:45 -0400

    Original Upload

---
+++ cc_computer/ccc_contact.php
@@ -0,0 +1,19 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<meta charset="utf-8">
+	<title>Contact Us</title>
+</head>
+<body>
+	<?php include("ccc_inc_header.php"); ?>
+	<h2 style="color:Blue; font-size: 2.5em;">Contact Us</h2>
+	<br/>
+	<br/>
+	<p>Coast City Computers</p>
+	<address>3601 Hempstead Turnpike<br/>Levittown NY 11756<br/>(516)796-1000<address>
+
+	<br/>
+	<a href="CC_Computer.php">Coast City Homepage</a>
+	<?php include("ccc_inc_footer.php"); ?>
+</body>
+</html>
---
+++ cc_computer/ccc_inc_footer.php
@@ -0,0 +1,11 @@
+<!-- This is the footer component of the CCComputers site -->
+<hr/>
+<table width=100% style="border: 0">
+	<tr>
+		<td><strong>Updated</strong>&nbsp;&nbsp;28 May, 2021</td>
+		<td style="text-align: right;">&copy; 2003 by Coast City Computers.</td>
+	</tr>
+
+
+
+</table>
